31368b322752f1a461f004d3a53ad01a25803fe1
vitorhugofsousa <vitorhugofs2018@gmail.com>
2026-03-10 23:39:34 -0300

    adding tasks for airflow

---
+++ dags/weather_dag.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import sys, os
+from datetime import datetime, timedelta
+from airflow.sdk import dag, task
+from pathlib import Path
+from dotenv import load_dotenv
+
+base_path = Path(__file__).resolve().parent.parent
+src_path = (base_path / 'src')
+if src_path not in sys.path:
+    sys.path.append(str(src_path))
+from etl_combined import extract_weather_data, data_transformation, load_data
+
+# Tenta o caminho relativo primeiro, depois um caminho absoluto fixo do Docker
+env_path = Path(__file__).resolve().parent.parent / 'config' / '.env'
+
+if not env_path.exists():
+    # Caminho padrão onde o Docker Compose mapeia o config
+    env_path = Path('/opt/airflow/config/.env')
+
+load_dotenv(dotenv_path=env_path)
+
+API_KEY = os.getenv('API_KEY')
+url = f"http://api.openweathermap.org/data/2.5/weather?q=Petrolina&units=metric&appid={API_KEY}&units=metric&"
+
+db_user = os.getenv("database")
+
+database = os.getenv('database')
+
+
+@dag(
+    dag_id="weather_etl_dag",
+    default_args={
+        'owner': 'airflow',
+        'depends_on_past': False,
+        'retries': 2,
+        'retry_delay': timedelta(minutes=5),
+    },
+    description="A DAG to extract, transform and load weather data into a PostgreSQL database",
+    schedule='0 */1 * * *',  # Run every hour
+    start_date=datetime(2026, 3, 10),
+    catchup=False,
+    tags=['weather', 'etl']
+)
+
+def weather_etl_dag():
+    @task()
+    def extract():
+         extract_weather_data(url)
+
+    @task()
+    def transform():
+        df = data_transformation()
+        df.to_parquet('/opt/airflow/data/temp_data.parquet', index=False)
+        return df
+
+    @task()
+    def load():
+        df = pd.read_parquet('/opt/airflow/data/temp_data.parquet')
+        load_data(database, df)
+
+    extract() >> transform() >> load()
+
+weather_etl_dag()
