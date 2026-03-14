5211af142d46dc38b7606ecb7357c5344b48181e
vitorhugofsousa <vitorhugofs2018@gmail.com>
2026-03-14 13:20:10 -0300

    aplicando a estrutura do terraform e atualizando o .gitignore para ignorar os arquivos do terraform

---
+++ terraform/main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "npipe:////./pipe/docker_engine"
+}
+
+locals {
+  # Lendo o .env e limpando aspas e caracteres de Windows (\r) apenas para o Terraform
+  env_raw   = file("${path.module}/../config/.env")
+  env_clean = replace(replace(replace(local.env_raw, "\r", ""), "\"", ""), "'", "")
+  
+  # Criando o mapa de variáveis ignorando espaços em branco
+  envs = { for line in split("\n", local.env_clean) : 
+            trimspace(split("=", line)[0]) => trimspace(split("=", line)[1]) 
+            if length(split("=", line)) == 2 
+         }
+}
+
+resource "docker_network" "weather_net" {
+  name = "weather_network"
+}
+
+resource "docker_container" "postgres_db" {
+  name  = "weather_postgres"
+  image = "postgres:18"
+  networks_advanced { name = docker_network.weather_net.name }
+  
+  env = [
+    "POSTGRES_USER=${local.envs["user"]}",
+    "POSTGRES_PASSWORD=${local.envs["password"]}",
+    "POSTGRES_DB=${local.envs["database"]}"
+  ]
+  
+  ports {
+    internal = 5432
+    external = 5444 # Mantemos 5444 para evitar o conflito com seu Postgres local
+  }
+}
