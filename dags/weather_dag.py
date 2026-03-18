61d0dab39f0a2b795e6506abcac5e962b20cd755
vitorhugofsousa <vitorhugofs2018@gmail.com>
2026-03-18 14:37:44 -0300

    Changing the API and the scope of the project. The Open-Meteo API is free and doesn't require an API key, but it has a different structure and endpoints compared to the previous API. The code has been updated to reflect these changes, including the extraction of historical weather data for the 27 Brazilian capitals, and the transformation of this data into a tabular format suitable for loading into a database. The logging has also been enhanced to provide more detailed information about the extraction and transformation processes.

--- dags/weather_dag.py
+++ dags/weather_dag.py
@@ -14,25 +14,17 @@ if str(src_path) not in sys.path:
 from etl_combined import extract_weather_data, data_transformation, load_data
 
 
-env_path = Path(__file__).resolve().parent.parent / 'config' / '.env'
-if not env_path.exists():
-    env_path = Path('/opt/airflow/config/.env')
-load_dotenv(dotenv_path=env_path)
-
-API_KEY = os.getenv('API_KEY')
-url = f"http://api.openweathermap.org/data/2.5/weather?q=Petrolina&units=metric&appid={API_KEY}"
-
 @dag(
-    dag_id="weather_etl_dag",
+    dag_id="weather_etl_dag1",
     default_args={
         'owner': 'airflow',
         'depends_on_past': False,
-        'retries': 2,
-        'retry_delay': timedelta(minutes=5),
+        'retries': 1,
+        'retry_delay': timedelta(minutes=2),
     },
     description="A DAG to extract, transform and load weather data into a PostgreSQL database",
-    schedule='0 */1 * * *',
-    start_date=datetime(2026, 3, 10),
+    schedule='@once',
+    start_date=datetime(2026, 3, 18),
     catchup=False,
     tags=['weather', 'etl']
 )
@@ -40,16 +32,15 @@ def weather_etl_dag():
 
     @task()
     def extract():
-         return extract_weather_data(url)
+        data_inicio = "2025-12-21"
+        data_fim = "2026-03-17"
+        return extract_weather_data(data_inicio, data_fim)
+
 
     @task()
     def transform(raw_data):
-        df = data_transformation(raw_data)
-        
-        temp_path = '/opt/airflow/data/temp_data.parquet'
-        os.makedirs(os.path.dirname(temp_path), exist_ok=True)
-        df.to_parquet(temp_path, index=False)
-        return temp_path 
+        path_parquet = data_transformation(raw_data)
+        return path_parquet
 
     @task()
     def load(file_path):
@@ -59,7 +50,7 @@ def weather_etl_dag():
 
 
     dados_brutos = extract()
-    caminho_parquet = transform(dados_brutos)
-    load(caminho_parquet)
+    path_parquet = transform(dados_brutos)
+    load(path_parquet)
 
 weather_etl_dag()
--- src/etl_combined.py
+++ src/etl_combined.py
@@ -9,143 +9,156 @@ import json
 from sqlalchemy import create_engine, text
 from urllib.parse import quote_plus
 import os
+import time
 from dotenv import load_dotenv
 from urllib.parse import quote_plus
 
 # export
 ## Extract data function
+
+CAPITAIS_COORDS = {
+    "Rio Branco": {"lat": -9.97499, "lon": -67.8243},
+    "Maceió": {"lat": -9.6658, "lon": -35.7353},
+    "Macapá": {"lat": 0.0389, "lon": -51.0664},
+    "Manaus": {"lat": -3.1019, "lon": -60.0250},
+    "Salvador": {"lat": -12.9714, "lon": -38.5014},
+    "Fortaleza": {"lat": -3.7172, "lon": -38.5431},
+    "Brasília": {"lat": -15.7801, "lon": -47.9292},
+    "Vitória": {"lat": -20.3155, "lon": -40.3128},
+    "Goiânia": {"lat": -16.6869, "lon": -49.2643},
+    "São Luís": {"lat": -2.5297, "lon": -44.3028},
+    "Cuiabá": {"lat": -15.6014, "lon": -56.0979},
+    "Campo Grande": {"lat": -20.4428, "lon": -54.6464},
+    "Belo Horizonte": {"lat": -19.9208, "lon": -43.9378},
+    "Belém": {"lat": -1.4558, "lon": -48.5044},
+    "João Pessoa": {"lat": -7.1150, "lon": -34.8631},
+    "Curitiba": {"lat": -25.4284, "lon": -49.2733},
+    "Recife": {"lat": -8.0476, "lon": -34.8770},
+    "Teresina": {"lat": -5.0892, "lon": -42.8016},
+    "Rio de Janeiro": {"lat": -22.9068, "lon": -43.1729},
+    "Natal": {"lat": -5.7945, "lon": -35.2110},
+    "Porto Alegre": {"lat": -30.0346, "lon": -51.2177},
+    "Porto Velho": {"lat": -8.7612, "lon": -63.9039},
+    "Boa Vista": {"lat": 2.8235, "lon": -60.6758},
+    "Florianópolis": {"lat": -27.5969, "lon": -48.5495},
+    "São Paulo": {"lat": -23.5505, "lon": -46.6333},
+    "Aracaju": {"lat": -10.9472, "lon": -37.0731},
+    "Palmas": {"lat": -10.2128, "lon": -48.3603}
+}
+
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def extract_weather_data(url:str) -> list:
-    response = requests.get(url)
-    data = response.json()
-
-
-    if response.status_code != 200:
-        logging.error(f"Failed to fetch data. Status code: {response.status_code}, Response: {data}")
-        return []
-
-    if not data:
-        logging.warning("No data found in the response.")
-        return []
-
-    output_path = Path("../data/weather_data.json")
-    output_dir = Path(output_path).parent
-    output_dir.mkdir(parents=True, exist_ok=True)
-
-
-    with open(output_path, "w") as f:
-        json.dump(data, f, indent=4)
+def extract_weather_data(start_date="2025-12-21", end_date="2026-03-17"):
+    all_weather_data = []
     
+    for city, coords in CAPITAIS_COORDS.items():
+        try:
+            url = (
+                f"https://archive-api.open-meteo.com/v1/archive?"
+                f"latitude={coords['lat']}&longitude={coords['lon']}&"
+                f"start_date={start_date}&end_date={end_date}&"
+                f"hourly=temperature_2m,relative_humidity_2m,precipitation,wind_speed_10m&"
+                f"timezone=America/Sao_Paulo"
+            )
+            
+            response = requests.get(url)
+            
+            if response.status_code == 200:
+                data = response.json()
+                data['city_name'] = city
+                all_weather_data.append(data)
+                logging.info(f"✅ Historical data extracted successfully: {city} (Lat: {coords['lat']}, Lon: {coords['lon']})")
+            else:
+                logging.error(f"❌ Error in API for {city}: {response.status_code} - {response.text}")
 
-    print(f"Data extracted and saved to {output_path}")
-    return data
+
+            time.sleep(1)
+            
+        except requests.exceptions.RequestException as e:
+            logging.error(f"Failure in the network request when trying to extract data from {city}: {e}")
+            
+    logging.info(f"Extraction finished. Total cities processed: {len(all_weather_data)}")
+    return all_weather_data
 
 
 
 # export
 ## Transform data functions
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+import pandas as pd
+import logging
+from pathlib import Path
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 if "__file__" in globals():
     project_root = Path(__file__).resolve().parent.parent
 else: 
     project_root = Path.cwd().parent if Path.cwd().name in ['src', 'notebooks', 'dags'] else Path.cwd()
 
-path_name = project_root / 'data' / 'weather_data.json' 
+
 parquet_path = project_root / 'data' / 'temp_data.parquet' 
-path_name.parent.mkdir(parents=True, exist_ok=True)
+parquet_path.parent.mkdir(parents=True, exist_ok=True)
 
 
-columns_to_drop = ['weather', 'weather_icon', 'sys.type']
-columns_names_to_rename = {
-    "base": "base",
-    "visibility": "visibility",
-    "dt": "datetime",
-    "timezone": "timezone",
-    "id": "city_id",
-    "name": "city_name",
-    "cod": "code",
-    "coord.lon": "longitude",
-    "coord.lat": "latitude",
-    "main.temp": "temperature",
-    "main.feels_like": "feels_like",
-    "main.temp_min": "temp_min",
-    "main.temp_max": "temp_max",
-    "main.pressure": "pressure",
-    "main.humidity": "humidity",
-    "main.sea_level": "sea_level",
-    "main.grnd_level": "grnd_level",
-    "wind.speed": "wind_speed",
-    "wind.deg": "wind_deg",
-    "wind.gust": "wind_gust",
-    "clouds.all": "clouds",
-    "sys.type": "sys_type",
-    "sys.id": "sys_id",
-    "sys.country": "country",
-    "sys.sunrise": "sunrise",
-    "sys.sunset": "sunset",
-}
-columns_to_normalize_date = ['datetime', 'sunrise', 'sunset']
+def data_transformation(raw_data_list):
 
-def create_dataframe(path_name:str) -> pd.DataFrame:
-
-    logging.info(f"Creating dataframe from {path_name}.json")
-    path = path_name
-
-    if not path.exists():
-        logging.error(f"File {path} does not exist or is not found.")
-
-    with open(path) as f:
-        data = json.load(f)
-
-    df= pd.json_normalize(data)
-    logging.info(f"Dataframe created successfully from {path_name}.json")
-    return df
-
-def normalize_weather_columns(df: pd.DataFrame) -> pd.DataFrame:
-    df_weather = pd.json_normalize(df['weather'].apply(lambda x: x[0]))
+    logging.info("Starting data transformation for Open-Meteo...")
     
-    df_weather = df_weather.rename(columns={
-        'id': 'weather_id',
-        'main': 'weather_main',
-        'description': 'weather_description',
-        'icon': 'weather_icon'
+    lista_dfs = []
+    
+    
+    for city_data in raw_data_list:
+        
+       
+        hourly_data = city_data.get('hourly', {})
+        if not hourly_data:
+            continue
+            
+        
+        df_cidade = pd.DataFrame(hourly_data)
+        
+       
+        df_cidade['city_name'] = city_data.get('city_name')
+        df_cidade['latitude'] = city_data.get('latitude')
+        df_cidade['longitude'] = city_data.get('longitude')
+        
+        lista_dfs.append(df_cidade)
+        
+    if not lista_dfs:
+        logging.error("No valid data to transform.")
+        return None
+        
+    
+    df_final = pd.concat(lista_dfs, ignore_index=True)
+    
+    
+    df_final = df_final.rename(columns={
+        'time': 'datetime',
+        'temperature_2m': 'temperature',
+        'relative_humidity_2m': 'humidity',
+        'wind_speed_10m': 'wind_speed',
+        'precipitation': 'precipitation'
     })
-
-    df = pd.concat([df, df_weather], axis=1)
-    logging.info("Weather columns normalized successfully.")
-    return df
-
-def drop_columns(df: pd.DataFrame, columns_names:list[str]) -> pd.DataFrame:
-    df = df.drop(columns=columns_names)
-    logging.info(f"Columns {columns_names} dropped successfully.")
-    return df
-   
-def rename_columns(df: pd.DataFrame) -> pd.DataFrame:
-    df = df.rename(columns=columns_names_to_rename)
-    logging.info("Columns renamed successfully.")
-    return df
-
-def normalize_date_columns(df: pd.DataFrame, columns_names:list[str]) -> pd.DataFrame:
-    for name in columns_names:
-        df[name] = pd.to_datetime(df[name], unit='s', utc=True).dt.tz_convert('America/Sao_Paulo')
-    logging.info(f"Date columns {columns_names} normalized successfully.")
-    return df
-
-
-def data_transformation(data):
-    print("Starting data transformation...")
-    df = create_dataframe(path_name)
-    df = normalize_weather_columns(df) 
-    df = drop_columns(df, columns_to_drop)
-    df = rename_columns(df)
-    df = normalize_date_columns(df, columns_to_normalize_date)
-    logging.info("Data transformation completed successfully.")
-    return df
-
-
+    
+    df_final['datetime'] = pd.to_datetime(df_final['datetime'])
+    
+    df_final['date'] = df_final['datetime'].dt.date
+    df_final['time'] = df_final['datetime'].dt.time
+    
+    
+    df_final = df_final.drop(columns=['datetime'])
+    
+    colunas_ordenadas = [
+        'date', 'time', 'temperature', 'humidity', 'precipitation', 
+        'wind_speed', 'city_name', 'latitude', 'longitude'
+    ]
+    df_final = df_final[colunas_ordenadas]
+    
+    logging.info(f"Data transformation completed successfully! Total rows: {len(df_final)}")
+    
+    df_final.to_parquet(parquet_path, index=False)
+    
+    return str(parquet_path)
 
 # export 
 ## Load data function
@@ -175,21 +188,25 @@ else:
     else:
         print(f"❌ Error: .env file does not exist in {env_path}")
 
-user = os.getenv("user").strip("'\"") 
-password = str(os.getenv("password") or "").strip("'\"")
-database = os.getenv("database").strip("'\"")
-host = os.getenv("host").strip("'\"")
+user = str(os.getenv("user", "")).strip("'\"") 
+password = str(os.getenv("password", "")).strip("'\"")
+database = str(os.getenv("database", "")).strip("'\"")
+host = str(os.getenv("host", "")).strip("'\"")
 
 def get_engine():
-        logging.info(f"Connecting to the database {database} at {host} with user {user}.")
-        connection_string = f"postgresql+psycopg2://{user}:{quote_plus(password)}@{host}:5432/{database}"
-        return create_engine(connection_string)
+    logging.info(f"Connecting to the database {database} at {host} with user {user}.")
+    connection_string = f"postgresql+psycopg2://{user}:{quote_plus(password)}@{host}:5432/{database}"
+    return create_engine(connection_string)
 
-engine = get_engine()
+
 
 def load_data(table_name:str, df):
-        df.to_sql(table_name, con=engine, if_exists='append', index=False)
-        logging.info(f"Data loaded successfully into the {table_name} table.")
-        df_check = pd.read_sql(text(f"SELECT * FROM {table_name}"), con=engine)
-        logging.info(f"Data in {table_name} table:{len(df_check)}\n")
+
+    engine = get_engine()
+    df.to_sql(table_name, con=engine, if_exists='append', index=False)
+    logging.info(f"Data loaded successfully into the {table_name} table.")
+    with engine.connect() as conn:
+        df_check = pd.read_sql(text(f"SELECT COUNT(*) as total FROM {table_name}"), con=conn)
+        total_linhas = df_check['total'].iloc[0]
+        logging.info(f"📊 Total registries in DB'{table_name}': {total_linhas}\n")
 
